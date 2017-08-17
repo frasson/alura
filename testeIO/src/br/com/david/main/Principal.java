2378a64c2317ee18d3583b8e4b0deb6e31a2101e
frasson <bharth@gmail.com>
2017-08-17 00:07:59 -0300

    Curso IO

--- testeIO/src/br/com/david/main/Principal.java
+++ testeIO/src/br/com/david/main/Principal.java
@@ -1,27 +1,18 @@
 package br.com.david.main;
 
-import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class Principal {
-
-	public static void main(String[] args) {
-		InputStream is = System.in;
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader bfr = new BufferedReader(isr);
-
-		String linha;
-		try {
-			linha = bfr.readLine();
-			while (linha != null) {
-				System.out.println("Linha: " + linha);
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws IOException {
+        Scanner entrada = new Scanner(System.in);
+        PrintStream saida = new PrintStream(new FileOutputStream("txts/copia.txt"));
+        while (entrada.hasNextLine()) {
+            saida.println(entrada.nextLine());
+        }
+        saida.close();
+        entrada.close();
 	}
-
 }
