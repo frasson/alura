8f740cdecfe0792a6abb5d5575c021f1fa701cf0
David Frasson Junior <david.junior@betha.com.br>
2017-09-20 13:59:13 -0300

    Fim aula 5

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp')
-    ,imagemin = require('gulp-imagemin')
-    ,clean = require('gulp-clean')
-    ,concat = require('gulp-concat')
-    ,htmlReplace = require('gulp-html-replace')
-    ,uglify = require('gulp-uglify')
-    ,usemin = require('gulp-usemin')
-    ,cssmin = require('gulp-cssmin')
-    ,browserSync = require('browser-sync');
+  ,imagemin = require('gulp-imagemin')
+  ,clean = require('gulp-clean')
+  ,concat = require('gulp-concat')
+  ,htmlReplace = require('gulp-html-replace')
+  ,uglify = require('gulp-uglify')
+  ,usemin = require('gulp-usemin')
+  ,cssmin = require('gulp-cssmin')
+  ,browserSync = require('browser-sync')
+  ,jshint = require('gulp-jshint')
+  ,jshintStylish = require('jshint-stylish')
+  ,csslint = require('gulp-csslint');
 
 /*
  * O segundo parâmetro é um array de dependência 
@@ -47,6 +50,15 @@ return gulp.src('dist/**/*.html')
   .pipe(gulp.dest('dist'));
 });
 
+gulp.task('usemin', function() {
+  return gulp.src('dist/**/*.html')
+    .pipe(usemin({
+      js: [uglify],
+      css: [cssmin]
+    }))
+    .pipe(gulp.dest('dist'));
+});
+
 gulp.task('server', function() {
   browserSync.init({
       server: {
@@ -55,4 +67,19 @@ gulp.task('server', function() {
   });
 
   gulp.watch('src/**/*').on('change', browserSync.reload);
+
+  gulp.watch('src/js/**/*.js').on('change', function(event) {
+      console.log("Linting " + event.path);
+      gulp.src(event.path)
+          .pipe(jshint())
+          .pipe(jshint.reporter(jshintStylish));
+  });
+
+  gulp.watch('src/css/**/*.css').on('change', function(event) {
+      console.log("Linting " + event.path);
+      gulp.src(event.path)
+          .pipe(csslint())
+          .pipe(csslint.reporter());
+  });    
+
 });
