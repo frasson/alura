2c195abdc51df637c4aff95fa1483adcba0122aa
David Frasson Junior <david.junior@betha.com.br>
2017-09-20 13:32:50 -0300

    Fim aula 4

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,29 +1,58 @@
-var gulp = require('gulp'), 
-    imagemin = require('gulp-imagemin'), 
-    clean = require('gulp-clean');
+var gulp = require('gulp')
+    ,imagemin = require('gulp-imagemin')
+    ,clean = require('gulp-clean')
+    ,concat = require('gulp-concat')
+    ,htmlReplace = require('gulp-html-replace')
+    ,uglify = require('gulp-uglify')
+    ,usemin = require('gulp-usemin')
+    ,cssmin = require('gulp-cssmin')
+    ,browserSync = require('browser-sync');
+
+/*
+ * O segundo parâmetro é um array de dependência 
+ * que deve ser executada antes dessa tarefa.
+ */
+gulp.task('default', ['copy'], function() {
+  gulp.start('build-img', 'usemin');
+});
 
 /**
  * Como o clean é uma dependência, utilizamos o return, 
  * assim garantimos que ela vai ser executada complementamente 
  * antes que a próxima tarefa seja executada.
  */    
-gulp.task('clean', function(){
-    return gulp.src('dist')
-        .pipe(clean());
+gulp.task('copy', ['clean'], function() {
+  return gulp.src('src/**/*')
+      .pipe(gulp.dest('dist'));
 });
 
-/*
- * O segundo parâmetro é um array de dependência 
- * que deve ser executada antes dessa tarefa.
- */
-gulp.task('copy', ['clean'],function(){
-    return gulp.src('src/**/*')
-        .pipe(gulp.dest('dist'));
+gulp.task('clean', function() {
+  return gulp.src('dist')
+      .pipe(clean());
 });
 
-gulp.task('build-img', ['copy'], function(){
+gulp.task('build-img', function() {
 
-    gulp.src('src/img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('src/img'));
+return gulp.src('dist/img/**/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', function() {
+return gulp.src('dist/**/*.html')
+  .pipe(usemin({
+    js: [uglify],
+    css: [cssmin]
+  }))
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('server', function() {
+  browserSync.init({
+      server: {
+          baseDir: 'src'
+      }
+  });
+
+  gulp.watch('src/**/*').on('change', browserSync.reload);
 });
