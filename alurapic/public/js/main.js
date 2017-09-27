b6134a06844c3c9bcf590865628f5692ab957a43
David Frasson Junior <david.junior@betha.com.br>
2017-09-27 15:09:44 -0300

    Fim aula 07

--- alurapic/public/js/main.js
+++ alurapic/public/js/main.js
@@ -1,16 +1,20 @@
 angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
-    .config( function($routeProvider, $locationProvider) {
+.config(function($routeProvider, $locationProvider) {
 
-        $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 
-        $routeProvider.when('/fotos', {
-            templateUrl: 'partials/principal.html',
-            controller: 'FotosController'
-        });
-
-        $routeProvider.when('/fotos/new', {
-            templateUrl: 'partials/foto.html',
-        });
-
-        $routeProvider.otherwise({redirectTo: '/fotos'});
+    $routeProvider.when('/fotos', {
+        templateUrl: 'partials/principal.html',
+        controller: 'FotosController'
     });
+
+    // adicionando a propriedade controller que faltava. 
+
+    $routeProvider.when('/fotos/new', {
+        templateUrl: 'partials/foto.html',
+        controller: 'FotoController'
+    });
+
+    $routeProvider.otherwise({redirectTo: '/fotos'});
+
+});
