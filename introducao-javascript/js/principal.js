9981acfffd192b79add5462988a79426a9d90202
David Frasson Junior <david.junior@betha.com.br>
2017-09-28 13:50:24 -0300

    Introdução ao JavaScript, fim aula 03

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,27 +1,38 @@
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdImc = paciente.querySelector(".info-imc");
+for(var i = 0; i < pacientes.length ; i++){
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
 
-var alturaEhValida = true;
-var pesoEhValido = true;
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
 
-if(peso <= 0 || peso > 1000){
-    console.log("Peso inválido");
-    tdImc.textContent = "Peso inválido!";
-    pesoEhValido = false;
-}
+    var alturaEhValida = true;
+    var pesoEhValido = true;
 
-if(altura <= 0 || altura >= 3){
-    console.log("Altura inválida");
-    tdImc.textContent = "Altura inválida!";
-    alturaEhValida = false;
-}
+    if(peso <= 0 || peso > 1000){
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
 
-if(pesoEhValido && alturaEhValida){
-    var imc = peso / ( altura * altura);
-    tdImc.textContent = imc;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura <= 0 || altura >= 1000){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var imc = peso / ( altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+
+
 }
