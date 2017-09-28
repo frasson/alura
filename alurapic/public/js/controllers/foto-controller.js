3ea396fdddcfe17108b83e9da080249a4f093565
frasson <bharth@gmail.com>
2017-09-27 23:49:23 -0300

    Fim aula 11

--- alurapic/public/js/controllers/foto-controller.js
+++ alurapic/public/js/controllers/foto-controller.js
@@ -1,5 +1,5 @@
 angular.module('alurapic')
-    .controller('FotoController', function($scope, recursoFoto, $routeParams) {
+    .controller('FotoController', function($scope, recursoFoto, $routeParams, cadastroDeFotos) {
 
         $scope.foto = {};
         $scope.mensagem = '';
@@ -8,7 +8,6 @@ angular.module('alurapic')
             recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto) {
                 $scope.foto = foto; 
             }, function(erro) {
-                console.log(erro);
                 $scope.mensagem = 'Não foi possível obter a foto'
             });
         }
@@ -16,27 +15,14 @@ angular.module('alurapic')
         $scope.submeter = function() {
 
             if ($scope.formulario.$valid) {
-
-                if($routeParams.fotoId) {
-
-                    recursoFoto.update({fotoId: $scope.foto._id}, 
-                        $scope.foto, function() {
-                        $scope.mensagem = 'Foto alterada com sucesso';
-                    }, function() {
-                        console.log(erro);
-                        $scope.mensagem = 'Não foi possível alterar';
-                    });
-
-                } else {
-                    recursoFoto.save($scope.foto, function() {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Foto cadastrada com sucesso';
-                    }, function(erro) {
-                        console.log(erro);
-                        $scope.mensagem = 'Não foi possível cadastrar a foto';
-                    });
-                }
+                cadastroDeFotos.cadastrar($scope.foto)
+                .then(function(dados) {
+                    $scope.mensagem = dados.mensagem;
+                    if (dados.inclusao) $scope.foto = {};
+                })
+                .catch(function(erro) {
+                    $scope.mensagem = erro.mensagem;
+                });
             }
         };
-
     });
